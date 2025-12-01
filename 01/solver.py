40dc551e3d3e1d1b59d65d2374f54dcd4c9b3d6e
Matt Clarke <matto.kuraku@gmail.com>
2025-12-01 07:20:06 +0100

    Update solver.py

--- 01/solver.py
+++ 01/solver.py
@@ -1,6 +1,4 @@
-import copy
 import sys
-from collections import deque
 
 
 FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
@@ -9,17 +7,39 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
 
 result = 0
+pos = 50
 
 for line in lines:
-    pass
+    d = line[0]
+    n = int(line[1:])
+    if d == "L":
+        pos -= n
+    else:
+        pos += n
+    pos %= 100
+    if pos == 0:
+        result += 1
 
-# Part 1 = 
+# Part 1 = 1055
 print(f"answer = {result}")
 
 result = 0
+pos = 50
+prev = True
 
-# Part 2 = 
+for line in lines:
+    d = line[0]
+    n = int(line[1:])
+    for i in range(n):
+        if d == "L":
+            pos -= 1
+        else:
+            pos += 1
+        pos %= 100
+        if pos == 0:
+            result += 1
+
+# Part 2 = 6368
 print(f"answer = {result}")
