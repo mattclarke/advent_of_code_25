5f94edd6f020183ba646929a9d718cb7a2cda8be
Matt Clarke <matto.kuraku@gmail.com>
2025-12-03 07:42:30 +0100

    Update solver.py

--- 03/solver.py
+++ 03/solver.py
@@ -1,6 +1,4 @@
-import copy
 import sys
-from collections import deque
 
 
 FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
@@ -8,18 +6,43 @@ FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
-lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
+temp = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
+
+lines = []
+for t in temp:
+    line = []
+    for ch in t:
+        line.append(int(ch))
+    lines.append(line)
 
 result = 0
 
 for line in lines:
-    pass
+    mx = max(line[:-1])
+    i = line.index(mx)
+    my = max(line[i+1:])
+    result += mx * 10 + my
 
-# Part 1 = 
+# Part 1 = 17281
 print(f"answer = {result}")
+
+
+def recurse(line, e, ans):
+    if e == 0:
+        return
+    a = len(line) - e + 1
+    m = max(line[:a])
+    i = line.index(m)
+    ans.append(m)
+    recurse(line[i+1:], e - 1, ans)
+
 
 result = 0
 
-# Part 2 = 
+for line in lines:
+    ans = []
+    recurse(line, 12, ans)
+    result += int(("".join(str(x) for x in ans)))
+
+# Part 2 = 171388730430281
 print(f"answer = {result}")
