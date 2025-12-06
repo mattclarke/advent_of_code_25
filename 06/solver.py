5b6c543f16cb205709c7e21ff48f1d19bd515224
Matt Clarke <matto.kuraku@gmail.com>
2025-12-06 07:30:09 +0100

    06: part 2 done

--- 06/solver.py
+++ 06/solver.py
@@ -1,6 +1,4 @@
-import copy
 import sys
-from collections import deque
 
 
 FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
@@ -9,7 +7,6 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
 
 cols = []
 for line in lines:
@@ -39,5 +36,45 @@ print(f"answer = {result}")
 
 result = 0
 
-# Part 2 = 
+operators = lines.pop().split()
+
+c = 0
+foo = []
+
+for c in range(len(lines[0])):
+    chars = []
+    for l in lines:
+        if l[~c] != " ":
+            chars.append(l[~c])
+    if chars:
+        foo.append(int("".join(chars)))
+    else:
+        op = operators.pop()
+        ans = foo.pop(0)
+        for f in foo:
+            if op == "*":
+                ans *= f
+            elif op == "+":
+                ans += f
+            else:
+                assert False
+
+        result += ans
+
+        foo = []
+    c += 1
+
+op = operators.pop()
+ans = foo.pop(0)
+for f in foo:
+    if op == "*":
+        ans *= f
+    elif op == "+":
+        ans += f
+    else:
+        assert False
+
+result += ans
+
+# Part 2 = 9434900032651
 print(f"answer = {result}")
