c0cdf4abf91793350327d90edd9a2b91d1f31221
Matt Clarke <matto.kuraku@gmail.com>
2025-12-07 08:35:08 +0100

    07: part 1 done

--- 07/solver.py
+++ 07/solver.py
@@ -9,17 +9,27 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
 
 result = 0
 
-for line in lines:
-    pass
+start = lines[0].index("S")
+beams = {start}
 
-# Part 1 = 
+for line in lines[1:]:
+    new_beams = set()
+    for b in beams:
+        if line[b] == "^":
+            result += 1
+            new_beams.add(b - 1)
+            new_beams.add(b + 1)
+        else:
+            new_beams.add(b)
+    beams = new_beams
+
+# Part 1 = 1640
 print(f"answer = {result}")
 
 result = 0
 
-# Part 2 = 
+# Part 2 =
 print(f"answer = {result}")
