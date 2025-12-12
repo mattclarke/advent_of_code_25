94a2d76891d8bf2f4a6ef186e94ecc059824d0df
Matt Clarke <matto.kuraku@gmail.com>
2025-12-12 06:58:02 +0100

    Update solver.py

--- 12/solver.py
+++ 12/solver.py
@@ -8,12 +8,32 @@ FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
-lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
+lines = [line.strip() for line in PUZZLE_INPUT.split("\n\n") if line]
+
+SHAPES = []
+AREAS = []
+ALLOWED = []
+
+for line in lines[:6]:
+    slines = line.split("\n")
+    shape = set()
+    for r, sline in enumerate(slines[1:]):
+        for c, ch in enumerate(sline):
+            if ch == "#":
+                shape.add((r, c))
+    SHAPES.append(shape)
+
+for line in lines[6].replace(":", "").split("\n"):
+    parts = line.split(" ")
+    area = tuple([int(x) for x in parts[0].split("x")])
+    allowances = tuple([int(x) for x in parts[1:]])
+    AREAS.append(area)
+    ALLOWED.append(allowances)
+
 
 result = 0
 
-for line in lines:
+for sline in lines:
     pass
 
 # Part 1 = 
