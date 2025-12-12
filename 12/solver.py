426632e49a01771620f1b1af80e1358e6d9e70e6
Matt Clarke <matto.kuraku@gmail.com>
2025-12-12 07:29:56 +0100

    Update solver.py

--- 12/solver.py
+++ 12/solver.py
@@ -31,10 +31,55 @@ for line in lines[6].replace(":", "").split("\n"):
     ALLOWED.append(allowances)
 
 
+# Symmetry means we don't need to try every starting position and/or rotation?
+
+def draw_shape(shape):
+    for r in range(3):
+        line = []
+        for c in range(3):
+            if (r, c) in shape:
+                line.append("#")
+            else:
+                line.append(".")
+        print("".join(line))
+    print("")
+
+
+def rotate_right(shape):
+    new_shape = set()
+    if (0, 0) in shape:
+        new_shape.add((0, 2))
+    if (0, 1) in shape:
+        new_shape.add((1, 2))
+    if (0, 2) in shape:
+        new_shape.add((2, 2))
+    if (1, 0) in shape:
+        new_shape.add((0, 1))
+    if (1, 1) in shape:
+        new_shape.add((1, 1))
+    if (1, 2) in shape:
+        new_shape.add((2, 1))
+    if (2, 0) in shape:
+        new_shape.add((0, 0))
+    if (2, 1) in shape:
+        new_shape.add((1, 0))
+    if (2, 2) in shape:
+        new_shape.add((2, 0))
+    return new_shape
+
+
 result = 0
 
-for sline in lines:
-    pass
+for area, allowed in zip(AREAS, ALLOWED):
+    allowed_shapes = []
+    total_shape_area = 0
+    for i, a in enumerate(allowed):
+        allowed_shapes.extend([SHAPES[i]] * a)
+        total_shape_area += len(SHAPES[i]) * a
+    if total_shape_area > area[0] * area[1]:
+        print("Not a chance")
+        continue
+
 
 # Part 1 = 
 print(f"answer = {result}")
