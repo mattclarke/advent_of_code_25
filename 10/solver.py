e2b37171f81015a9acc31949bed1a8530e17c1bb
Matt Clarke <matto.kuraku@gmail.com>
2025-12-13 10:29:36 +0100

    10: works for example

--- 10/solver.py
+++ 10/solver.py
@@ -63,7 +63,47 @@ for data in input_data:
 # Part 1 = 505
 print(f"answer = {result}")
 
+# Puzzles are:
+#     (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+# First part can be written as:
+#     (3)    => [0, 0, 0, 1]
+#     (1, 3) => [0, 1, 0, 1]
+#     ...
+#     (0, 1) => [1, 1, 0, 0]
+# So the puzzle is:
+#     A x [0, 0, 0, 1] + B x [0, 1, 0, 1] + .. + F x [1, 1, 0, 0] = {3,5,4,7}
+# To solve we need to find A, B, C, etc.
+
 result = 0
+
+for data in input_data:
+    _, switches, jolts = data
+    state = [0 for _ in jolts]
+    Q = [(0, state)]
+    # Don't revist seen states
+    seen = set()
+    while Q:
+        depth, state = Q.pop(0)
+        print(state)
+        if state == jolts:
+            print(depth)
+            result += depth
+            break
+        if tuple(state) in seen:
+            continue
+        seen.add(tuple(state))
+        for s in switches:
+            new_state = state.copy()
+            invalid = False
+            for i in s:
+                new_state[i] += 1
+                if new_state[i] > jolts[i]:
+                    invalid = True
+                    break
+            if invalid:
+                continue
+
+            Q.append((depth + 1, new_state))
 
 # Part 2 = 
 print(f"answer = {result}")
