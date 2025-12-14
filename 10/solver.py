5b0dc2317f62cd3f4fd53d22451530dddcbbc0d2
Matt Clarke <matto.kuraku@gmail.com>
2025-12-14 11:03:28 +0100

    10: wip works for example

--- 10/solver.py
+++ 10/solver.py
@@ -82,8 +82,75 @@ print(f"answer = {result}")
 # For 3:
 #     A + B + D = 7
 # That should help limit the options...
+# But how to code it...
+# Guess E = 2 => F = 1 => B = 4 => A + D = 3 => C + D = 2
+# Guess D = 1 => A = 2 and C = 1
+# 0 0 0 2
+# 0 4 0 4
+# 0 0 1 0
+# 0 0 1 1
+# 2 0 2 0
+# 1 1 0 0
+# -------
+# 3 5 4 7 
+# Correct but depth is 11, so there is a better solution...
+#
+# Try D = 2 => A = 1 and C = 0
+# 0 0 0 1
+# 0 4 0 4
+# 0 0 0 0
+# 0 0 2 2
+# 2 0 2 0
+# 1 1 0 0
+# -------
+# 3 5 4 7 
+# Depth = 10 which is the lowest.
+
+
+def is_valid(candidates, foo, jolts):
+    for k, v in foo.items():
+        bar = []
+        for vv in v:
+            bar.append(candidates[vv])
+        if sum(bar) != jolts[k]:
+            return False
+    return True
+
+
+best = 1000000000000
+    
+def solve(foo, switches, jolts):
+    global best
+    def recurse(i, candidates):
+        global best
+        if i >= len(candidates):
+            if is_valid(candidates, foo, jolts):
+                best = min(best, sum(candidates))
+                print("here", candidates, sum(candidates), best)
+            return
+        # print(i, candidates)
+        for x in range(0, max(jolts)):
+            candidates[i] = x
+            recurse(i+1, candidates)
+    best = 1000000000000
+    recurse(0, [0 for _ in switches])
+    print(best)
+    return best
 
 result = 0
+
+for _, switches, jolts in input_data:
+    foo = {x: set() for x in range(len(jolts))}
+    for i, switch in enumerate(switches):
+        for s in switch:
+            foo[s].add(i)
+
+    print(foo)
+    # print(is_valid({0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1}, foo, jolts))
+    # print(is_valid({0: 2, 1: 4, 2: 1, 3: 1, 4: 2, 5: 1}, foo, jolts))
+    # print(is_valid({0: 1, 1: 4, 2: 0, 3: 2, 4: 2, 5: 1}, foo, jolts))
+
+    result += solve(foo, switches, jolts)
 
 # Part 2 = 
 print(f"answer = {result}")
